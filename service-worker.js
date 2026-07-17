0560f629b413107c0dc87788695be88dea1f6cde
27022023 <252882813+27022023@users.noreply.github.com>
2026-07-17 18:02:18 +0200

    Rename cache from 'stellar lab-cache' to 'stellarlab-cache'

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("stellar lab-cache").then(cache => {
+    caches.open("stellarlab-cache").then(cache => {
       return cache.addAll([
         "index.html",
         "explore.html",
